535d3be7821df4a3ff293e6b3e9ca342072ac157
Ryan Lazenby <ryanlazenby77@gmail.com>
2022-04-08 23:29:45 -0700

    Final fixes

--- group3package/src/group3package/__init__.py
+++ group3package/src/group3package/__init__.py
@@ -2,4 +2,4 @@
 from importlib.metadata import version
 __version__ = version("group3package")
 
-from . import download_data, filter, process_data, regression, remove_column, summary_graph, visualize_data
+from . import filter, regression, remove_column
